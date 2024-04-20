d1011c80f2576b5d4227a26827092c4937673ca9
white <pavel1712171@gmail.com>
2024-04-20 21:33:39 +0300

    Comments and optimization

--- WFBlackjack/Form1.cs
+++ WFBlackjack/Form1.cs
@@ -1,324 +1,373 @@
-namespace WFBlackjack
+namespace WFBlackjack;
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    const int standartDelayMs = 300;
+
+    List<PlayingCard> cards;
+    List<PlayingCard> playerCards;
+    List<PlayingCard> croupierCards;
+
+    List<PictureBox> croupCardPicBoxes;
+    List<PictureBox> playerCardPicBoxes;
+
+    public Form1()
     {
-        const int standartDelayMs = 300;
+        InitializeComponent();
+        cards = PlayingCardsHandler.GetSetOfCards();
+        playerCards = new List<PlayingCard>();
+        croupierCards = new List<PlayingCard>();
 
-        List<PlayingCard> cards;
-        List<PlayingCard> playerCards;
-        List<PlayingCard> croupierCards;
-
-        List<PictureBox> croupCardPicBoxes;
-        List<PictureBox> playerCardPicBoxes;
-
-        public Form1()
+        croupCardPicBoxes = new List<PictureBox>
         {
-            InitializeComponent();
-            cards = PlayingCardsHandler.GetSetOfCards();
-            playerCards = new List<PlayingCard>();
-            croupierCards = new List<PlayingCard>();
+            CroupCardPicBox1,
+            CroupCardPicBox2,
+            CroupCardPicBox3,
+            CroupCardPicBox4,
+            CroupCardPicBox5,
+            CroupCardPicBox6,
+        };
 
-            croupCardPicBoxes = new List<PictureBox>
-            {
-                CroupCardPicBox1,
-                CroupCardPicBox2,
-                CroupCardPicBox3,
-                CroupCardPicBox4,
-                CroupCardPicBox5,
-                CroupCardPicBox6,
-            };
-
-            playerCardPicBoxes = new List<PictureBox>
-            {
-                PlayerCardPicBox1,
-                PlayerCardPicBox2,
-                PlayerCardPicBox3,
-                PlayerCardPicBox4,
-                PlayerCardPicBox5,
-                PlayerCardPicBox6,
-            };
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
+        playerCardPicBoxes = new List<PictureBox>
         {
-            HitBtn.Enabled = false;
-            StandBtn.Enabled = false;
-            StartBtn.Enabled = true;
-            BustOrWinLabel.Text = string.Empty;
-            CenterAllLabels();
-        }
+            PlayerCardPicBox1,
+            PlayerCardPicBox2,
+            PlayerCardPicBox3,
+            PlayerCardPicBox4,
+            PlayerCardPicBox5,
+            PlayerCardPicBox6,
+        };
+    }
 
-        private async void StartBtn_Click(object sender, EventArgs e)
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        HitBtn.Enabled = false;
+        StandBtn.Enabled = false;
+        StartBtn.Enabled = true;
+        BustOrWinLabel.Text = string.Empty;
+        CenterAllLabels();
+    }
+
+    private async void StartBtn_Click(object sender, EventArgs e)
+    {
+        InitRound();
+        RoundSwitchButtons();
+
+        await Task.Run(() =>
         {
-            InitRound();
-            RoundSwitchButtons();
-
-            await Task.Run(() =>
+            this.Invoke(async () =>
             {
-                this.Invoke(async () =>
-                {
-                    CroupHit();
-                    await Task.Delay(standartDelayMs);
-                    Hit();
-                    await Task.Delay(standartDelayMs);
-                    CroupHit();
-                    await Task.Delay(standartDelayMs);
-                    Hit();
-                });
+                CroupHit();
+                await Task.Delay(standartDelayMs);
+                Hit();
+                await Task.Delay(standartDelayMs);
+                CroupHit();
+                await Task.Delay(standartDelayMs);
+                Hit();
             });
+        });
 
-            int croupCount = CountCards(croupierCards);
-            int playerCount = CountCards(playerCards);
-            if (croupCount == 21)
-            {
-                BustOrWinLabel.Text = $"Croupier have Blackjack. You bust down!";
-                NewRoundSwitchButtons();
-            }
-            else if (playerCount == 21)
-            {
-                BustOrWinLabel.Text = $"You have Blackjack. You won!";
-                NewRoundSwitchButtons();
-            }
-
-            CenterAllLabels();
-        }
-
-        private void HitBtn_Click(object sender, EventArgs e)
+        int croupCount = CountCards(croupierCards);
+        int playerCount = CountCards(playerCards);
+        if (croupCount == 21)
         {
-            Hit();
+            BustOrWinLabel.Text = $"Croupier have Blackjack. You bust down!";
+            NewRoundSwitchButtons();
         }
-
-        private async void StandBtn_Click(object sender, EventArgs e)
+        else if (playerCount == 21)
         {
-            HitBtn.Enabled = false;
-            StandBtn.Enabled = false;
-            await CoupierHitsAfterStandAsync();
-            CompareCount();
+            BustOrWinLabel.Text = $"You have Blackjack. You won!";
             NewRoundSwitchButtons();
         }
 
-        private void Hit()
-        {
-            PlayingCard card = DealCard(playerCards);
-            SetPlayerCard(card);
-            int count = CountCards(playerCards);
-            if (count == 21)
-            {
-                BustOrWinLabel.Text = $"You have 21. You won!";
-                NewRoundSwitchButtons();
-            }
-            if (count > 21)
-            {
-                BustOrWinLabel.Text = $"You have {count}. You bust down!";
-                NewRoundSwitchButtons();
-            }
-            PlayerStatusLabel.Text = $"Your count: {count}";
+        CenterAllLabels();
+    }
 
-            CenterAllLabels();
+    private void HitBtn_Click(object sender, EventArgs e)
+    {
+        Hit();
+    }
+
+    private async void StandBtn_Click(object sender, EventArgs e)
+    {
+        HitBtn.Enabled = false;
+        StandBtn.Enabled = false;
+        await CoupierHitsAfterStandAsync();
+        CompareCountAndSetInfo();
+        NewRoundSwitchButtons();
+    }
+
+    /// <summary>
+    /// Hit of user(take a card)
+    /// </summary>
+    private void Hit()
+    {
+        PlayingCard card = DealCard(playerCards);
+        SetPlayerCard(card);
+        int count = CountCards(playerCards);
+        if (count == 21)
+        {
+            BustOrWinLabel.Text = $"You have 21. You won!";
+            NewRoundSwitchButtons();
+        }
+        if (count > 21)
+        {
+            BustOrWinLabel.Text = $"You have {count}. You bust down!";
+            NewRoundSwitchButtons();
+        }
+        PlayerStatusLabel.Text = $"Your count: {count}";
+
+        CenterAllLabels();
+    }
+
+    /// <summary>
+    /// Hit of bot(take a card)
+    /// </summary>
+    private void CroupHit()
+    {
+        PlayingCard card = DealCard(croupierCards);
+        card.IsReversed = croupierCards.Count == 2;
+        SetCroupierCard(card);
+
+        int count = CountCards(croupierCards);
+        if (count == 21)
+        {
+            BustOrWinLabel.Text = $"Croupier have 21. You bust down!";
+            NewRoundSwitchButtons();
+        }
+        if (count > 21)
+        {
+            BustOrWinLabel.Text = $"Croupier have {count}. You won!";
+            NewRoundSwitchButtons();
+        }
+        CroupierStatusLabel.Text = $"Croupier count: {count}";
+
+        CenterAllLabels();
+    }
+
+    /// <summary>
+    /// Initalize the round of game
+    /// </summary>
+    private void InitRound()
+    {
+        ResetPictures();
+        playerCards = new List<PlayingCard>();
+        croupierCards = new List<PlayingCard>();
+        cards = PlayingCardsHandler.GetSetOfCards();
+        PlayingCardsHandler.Shuffle(cards);
+        BustOrWinLabel.Text = string.Empty;
+    }
+
+    /// <summary>
+    /// Switch buttons enable after round ended or didnt started
+    /// </summary>
+    private void NewRoundSwitchButtons()
+    {
+        HitBtn.Enabled = false;
+        StandBtn.Enabled = false;
+        StartBtn.Enabled = true;
+    }
+
+    /// <summary>
+    /// Switch buttons enable after round started
+    /// </summary>
+    private void RoundSwitchButtons()
+    {
+        HitBtn.Enabled = true;
+        StandBtn.Enabled = true;
+        StartBtn.Enabled = false;
+    }
+
+    /// <summary>
+    /// Bot make hits to try win user
+    /// </summary>
+    /// <returns></returns>
+    private async Task CoupierHitsAfterStandAsync()
+    {
+        PlayingCard secondCard = croupierCards[1];
+        ReverseReversedCard(secondCard);
+        await Task.Delay(standartDelayMs);
+
+        int croupCount = CountCards(croupierCards);
+        int playerCount = CountCards(playerCards);
+
+        CroupierStatusLabel.Text = $"Croupier count: {croupCount}";
+
+        if (croupCount > playerCount)
+        {
+            return;
         }
 
-        private void CroupHit()
+        while (croupCount < 17)
         {
-            PlayingCard card = DealCard(croupierCards);
-            card.IsReversed = croupierCards.Count == 2;
-            SetCroupierCard(card);
-
-            int count = CountCards(croupierCards);
-            if (count == 21)
-            {
-                BustOrWinLabel.Text = $"Croupier have 21. You bust down!";
-                NewRoundSwitchButtons();
-            }
-            if (count > 21)
-            {
-                BustOrWinLabel.Text = $"Croupier have {count}. You won!";
-                NewRoundSwitchButtons();
-            }
-            CroupierStatusLabel.Text = $"Croupier count: {count}";
-
-            CenterAllLabels();
-        }
-
-        private void InitRound()
-        {
-            ResetPictures();
-            playerCards = new List<PlayingCard>();
-            croupierCards = new List<PlayingCard>();
-            cards = PlayingCardsHandler.GetSetOfCards();
-            PlayingCardsHandler.Shuffle(cards);
-            BustOrWinLabel.Text = string.Empty;
-        }
-
-        private void NewRoundSwitchButtons()
-        {
-            HitBtn.Enabled = false;
-            StandBtn.Enabled = false;
-            StartBtn.Enabled = true;
-        }
-
-        private void RoundSwitchButtons()
-        {
-            HitBtn.Enabled = true;
-            StandBtn.Enabled = true;
-            StartBtn.Enabled = false;
-        }
-
-        private async Task CoupierHitsAfterStandAsync()
-        {
-            PlayingCard secondCard = croupierCards[1];
-            ReverseReversedCard(secondCard);
-            await Task.Delay(standartDelayMs);
-            int croupCount = CountCards(croupierCards);
-            int playerCount = CountCards(playerCards);
-
-            CroupierStatusLabel.Text = $"Croupier count: {croupCount}";
+            croupCount = CountCards(croupierCards);
 
             if (croupCount > playerCount)
             {
                 return;
             }
 
-            while (croupCount < 17)
-            {
-                croupCount = CountCards(croupierCards);
+            CroupHit();
+            await Task.Delay(standartDelayMs);
+        }
+    }
 
-                if (croupCount > playerCount)
-                {
-                    return;
-                }
-
-                CroupHit();
-                await Task.Delay(standartDelayMs);
-            }
+    /// <summary>
+    /// Compares the counts of bot and user then put the text to label
+    /// </summary>
+    private void CompareCountAndSetInfo()
+    {
+        int p = CountCards(playerCards);
+        int c = CountCards(croupierCards);
+        if (c > 21)
+        {
+            BustOrWinLabel.Text = $"Croupier have {c}. You won!";
+        }
+        else if (p > 21)
+        {
+            BustOrWinLabel.Text = $"You have {p}. You bust down!";
+        }
+        else if (p > c)
+        {
+            BustOrWinLabel.Text = $"You have {p}. You won!";
+        }
+        else if (p < c)
+        {
+            BustOrWinLabel.Text = $"Croupier have {c}. You bust down!";
+        }
+        else
+        {
+            BustOrWinLabel.Text = $"You and Croupier have the same count. Tie!";
         }
 
-        private void CompareCount()
-        {
-            int p = CountCards(playerCards);
-            int c = CountCards(croupierCards);
-            if (c > 21)
-            {
-                BustOrWinLabel.Text = $"Croupier have {c}. You won!";
-            }
-            else if (p > 21)
-            {
-                BustOrWinLabel.Text = $"You have {p}. You bust down!";
-            }
-            else if (p > c)
-            {
-                BustOrWinLabel.Text = $"You have {p}. You won!";
-            }
-            else if (p < c)
-            {
-                BustOrWinLabel.Text = $"Croupier have {c}. You bust down!";
-            }
-            else
-            {
-                BustOrWinLabel.Text = $"You and Croupier have the same count. Tie!";
-            }
+        CenterAllLabels();
+    }
 
-            CenterAllLabels();
+    /// <summary>
+    /// Revese card that was reversed(second card of croupier)
+    /// </summary>
+    /// <param name="card"></param>
+    private void ReverseReversedCard(PlayingCard card)
+    {
+        if (!card.IsReversed) return;
+
+        card.IsReversed = false;
+        croupCardPicBoxes[1].Image = PlayingCardsHandler.GetImageByName(card.ResourceName);
+    }
+
+    /// <summary>
+    /// Just resets pictures
+    /// </summary>
+    private void ResetPictures()
+    {
+        foreach (var pic in croupCardPicBoxes)
+        {
+            pic.Image = null;
         }
 
-        private void ReverseReversedCard(PlayingCard card)
+        foreach (var pic in playerCardPicBoxes)
         {
-            if (!card.IsReversed) return;
+            pic.Image = null;
+        }
+    }
 
-            card.IsReversed = false;
-            croupCardPicBoxes[1].Image = PlayingCardsHandler.GetImageByName(card.ResourceName);
+    /// <summary>
+    /// Set images to table of bot cards
+    /// </summary>
+    /// <param name="card"></param>
+    private void SetCroupierCard(PlayingCard card)
+    {
+        if (croupierCards.Count == 2)
+        {
+            croupCardPicBoxes[1].Image = PlayingCardsHandler.GetImageByName("card_back_red");
+            return;
         }
 
-        private void ResetPictures()
+        foreach (var c in croupCardPicBoxes)
         {
-            foreach (var pic in croupCardPicBoxes)
+            if (c.Image == null)
             {
-                pic.Image = null;
-            }
-
-            foreach (var pic in playerCardPicBoxes)
-            {
-                pic.Image = null;
-            }
-        }
-
-        private void SetCroupierCard(PlayingCard card)
-        {
-            if (croupierCards.Count == 2)
-            {
-                croupCardPicBoxes[1].Image = PlayingCardsHandler.GetImageByName("card_back_red");
+                c.Image = PlayingCardsHandler.GetImageByName(card.ResourceName);
                 return;
             }
+        }
+    }
 
-            foreach (var c in croupCardPicBoxes)
+    /// <summary>
+    /// Set images to table of user cards
+    /// </summary>
+    /// <param name="card"></param>
+    private void SetPlayerCard(PlayingCard card)
+    {
+        foreach (var c in playerCardPicBoxes)
+        {
+            if (c.Image == null)
             {
-                if (c.Image == null)
-                {
-                    c.Image = PlayingCardsHandler.GetImageByName(card.ResourceName);
-                    return;
-                }
+                c.Image = PlayingCardsHandler.GetImageByName(card.ResourceName);
+                return;
             }
         }
+    }
 
-        private void SetPlayerCard(PlayingCard card)
+    /// <summary>
+    /// Gives 1 card to set
+    /// </summary>
+    /// <param name="setOfCard"></param>
+    /// <returns>Given playing card</returns>
+    private PlayingCard DealCard(List<PlayingCard> setOfCard)
+    {
+        var card = cards.First();
+        setOfCard.Add(card);
+        cards.Remove(card);
+        
+        return card;
+    }
+
+    private void CenterLabel(Label label)
+    {
+        label.Location = new Point((this.ClientSize.Width - label.Width) / 2, label.Top);
+    }
+
+    /// <summary>
+    /// Center all main labels
+    /// </summary>
+    private void CenterAllLabels()
+    {
+        CenterLabel(BustOrWinLabel);
+        CenterLabel(CroupierStatusLabel);
+        CenterLabel(PlayerStatusLabel);
+    }
+
+    /// <summary>
+    /// Counts set values of cards
+    /// </summary>
+    /// <param name="cards"></param>
+    /// <returns></returns>
+    private int CountCards(List<PlayingCard> cards)
+    {
+        int count = 0;
+        int aceCount = 0;
+
+        foreach (var card in cards)
         {
-            foreach (var c in playerCardPicBoxes)
+            if(card.IsReversed && cards.Count == 2)
             {
-                if (c.Image == null)
-                {
-                    c.Image = PlayingCardsHandler.GetImageByName(card.ResourceName);
-                    return;
-                }
-            }
-        }
-
-        private PlayingCard DealCard(List<PlayingCard> setOfCard)
-        {
-            var card = cards.First();
-            setOfCard.Add(card);
-            cards.Remove(card);
-            
-            return card;
-        }
-
-        private void CenterLabel(Label label)
-        {
-            label.Location = new Point((this.ClientSize.Width - label.Width) / 2, label.Top);
-        }
-
-        private void CenterAllLabels()
-        {
-            CenterLabel(BustOrWinLabel);
-            CenterLabel(CroupierStatusLabel);
-            CenterLabel(PlayerStatusLabel);
-        }
-
-        private int CountCards(List<PlayingCard> cards)
-        {
-            int count = 0;
-            int aceCount = 0;
-
-            foreach (var card in cards)
-            {
-                if(card.IsReversed && cards.Count == 2)
-                {
-                    continue;
-                }
-
-                if (card.ValueOfCard == 11)
-                {
-                    aceCount++;
-                }
-
-                count += card.ValueOfCard;
+                continue;
             }
 
-            while (count > 21 && aceCount > 0)
+            if (card.ValueOfCard == 11)
             {
-                count -= 10;
-                aceCount--;
+                aceCount++;
             }
 
-            return count;
+            count += card.ValueOfCard;
         }
+
+        while (count > 21 && aceCount > 0)
+        {
+            count -= 10;
+            aceCount--;
+        }
+
+        return count;
     }
 }
--- WFBlackjack/PlayingCardsHandler.cs
+++ WFBlackjack/PlayingCardsHandler.cs
@@ -1,97 +1,98 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
-
-namespace WFBlackjack
+﻿namespace WFBlackjack;
+public class PlayingCardsHandler
 {
-    public class PlayingCardsHandler
+    /// <summary>
+    /// Returns 52 standart french cards
+    /// </summary>
+    /// <returns></returns>
+    public static List<PlayingCard> GetSetOfCards()
     {
-        /// <summary>
-        /// Returns 52 standart french cards
-        /// </summary>
-        /// <returns></returns>
-        public static List<PlayingCard> GetSetOfCards()
+        List<PlayingCard> setOfCards = new List<PlayingCard>();
+
+        string[] suits = new string[]
         {
-            List<PlayingCard> setOfCards = new List<PlayingCard>();
+            "clubs", "diamonds", "hearts", "spades"
+        };
 
-            string[] suits = new string[]
-            {
-                "clubs", "diamonds", "hearts", "spades"
-            };
+        string[] pos = new string[]
+        {
+            "jack", "queen", "king", "ace"
+        };
 
-            string[] pos = new string[]
-            {
-                "jack", "queen", "king", "ace"
-            };
+        string[] posCapitilized = new string[]
+        {
+            "Jack", "Queen", "King", "Ace"
+        };
 
-            string[] posCapitilized = new string[]
+        for (int i = 2; i <= 10; i++)
+        {
+            for (int j = 0; j < suits.Length; j++)
             {
-                "Jack", "Queen", "King", "Ace"
-            };
-
-            for (int i = 2; i <= 10; i++)
-            {
-                for (int j = 0; j < suits.Length; j++)
+                setOfCards.Add(new PlayingCard()
                 {
-                    setOfCards.Add(new PlayingCard()
-                    {
-                        Name = $"{i} of {suits[j]}",
-                        ResourceName = $"_{i}_of_{suits[j]}",
-                        ValueOfCard = i,
-                        IsReversed = false
-                    });
-                }
+                    Name = $"{i} of {suits[j]}",
+                    ResourceName = $"_{i}_of_{suits[j]}",
+                    ValueOfCard = i,
+                    IsReversed = false
+                });
             }
+        }
 
-            for (int i = 0; i < pos.Length; i++)
+        for (int i = 0; i < pos.Length; i++)
+        {
+            for (int j = 0; j < suits.Length; j++)
             {
-                for (int j = 0; j < suits.Length; j++)
+                var cardObj = new PlayingCard()
                 {
-                    var cardObj = new PlayingCard()
-                    {
-                        Name = $"{posCapitilized[i]} of {suits[j]}",
-                        ResourceName = $"{pos[i]}_of_{suits[j]}",
-                        ValueOfCard = 10,
-                        IsReversed = false
-                    };
+                    Name = $"{posCapitilized[i]} of {suits[j]}",
+                    ResourceName = $"{pos[i]}_of_{suits[j]}",
+                    ValueOfCard = 10,
+                    IsReversed = false
+                };
 
-                    if (pos[i] == "ace")
-                    {
-                        cardObj.ValueOfCard = 11;
-                    }
-
-                    setOfCards.Add(cardObj);
+                if (pos[i] == "ace")
+                {
+                    cardObj.ValueOfCard = 11;
                 }
-            }
 
-            return setOfCards;
-        }
-
-
-        public static void Shuffle<T>(List<T> cards)
-        {
-            Random rng = new Random();
-            int n = cards.Count;
-            while (n > 1)
-            {
-                n--;
-                int k = rng.Next(n + 1);
-                T value = cards[k];
-                cards[k] = cards[n];
-                cards[n] = value;
+                setOfCards.Add(cardObj);
             }
         }
 
-        public static Bitmap GetImageByName(string imageName)
-        {
-            System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
-            string resourceName = asm.GetName().Name + ".Properties.Resources";
-            var rm = new System.Resources.ResourceManager(resourceName, asm);
-            return (Bitmap)rm.GetObject(imageName);
+        return setOfCards;
+    }
 
+
+    /// <summary>
+    /// Shuffles the cards
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="cards"></param>
+    public static void Shuffle<T>(List<T> cards)
+    {
+        Random rng = new Random();
+        int n = cards.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = cards[k];
+            cards[k] = cards[n];
+            cards[n] = value;
         }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="imageName"></param>
+    /// <returns>Image from resources by its name</returns>
+    public static Bitmap GetImageByName(string imageName)
+    {
+        System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
+        string resourceName = asm.GetName().Name + ".Properties.Resources";
+        var rm = new System.Resources.ResourceManager(resourceName, asm);
+        return (Bitmap)rm.GetObject(imageName);
+
     }
 }
